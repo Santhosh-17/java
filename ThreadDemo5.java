a19822ebc6e148bb8b566cdd13ae4279da8909ad
Santhosh Cholak <51264445+santhosh2401@users.noreply.github.com>
2019-09-14 18:40:49 +0530

    Add files via upload

---
+++ ThreadDemo5.java
@@ -0,0 +1,51 @@
+package Multi_Threading;
+class Callme
+{
+	void call(String msg)
+	{
+		System.out.print("["+msg);
+		try {
+			Thread.sleep(1000);
+		}catch(InterruptedException e) {}
+		System.out.println("]");
+	}
+}
+class Caller implements Runnable
+{
+	String msg;
+	Callme target;
+	Thread t;
+	public Caller (Callme targ,String s)
+	{
+		target = targ;
+		msg = s;
+		t = new Thread(this);
+		t.start();
+	}
+	public void run()
+	{
+		synchronized(target) {
+			target.call(msg);
+		}
+		
+	}
+}
+
+public class ThreadDemo5 {
+
+	public static void main(String[] args) {
+		Callme target = new Callme();
+		Caller ob1 = new Caller(target,"Hello");
+		Caller ob2 = new Caller(target,"synchronized");
+		Caller ob3 = new Caller(target,"World");
+
+		try
+		{
+			ob1.t.join();
+			ob2.t.join();
+			ob3.t.join();
+		}catch(InterruptedException e) {}
+		
+	}
+
+}
