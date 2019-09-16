7866394b84c69317b7da468d1b1208aacfa347fd
Santhosh Cholak <51264445+santhosh2401@users.noreply.github.com>
2019-09-16 22:01:22 +0530

    Add files via upload

---
+++ Home Works/Swing/SwingDemo2.java
@@ -0,0 +1,42 @@
+import java.awt.*;
+import javax.swing.*;
+public class SwingDemo2 {
+
+	public static void main(String[] args) {
+		JFrame f = new JFrame("Demo2");
+		JLabel l = new JLabel("Hello World");
+		
+		f.setLayout(new FlowLayout() );
+		f.setSize(300,300);
+		f.setVisible(true);
+		f.add(l);
+		
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+
+}
+
+/*
+import java.awt.*;
+import javax.swing.*;
+public class SwingDemo2 {
+	public static void main(String[] args) {
+		 new A();	
+	}
+}
+@SuppressWarnings("serial")
+class A extends JFrame
+{
+	A()
+	{
+		super("Demo1");
+		JLabel l = new JLabel("Hello World");
+		setLayout(new FlowLayout() );
+		setVisible(true); //Methods in JFrame
+		setSize(400,400); //Methods in JFrame
+		add(l);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
+*/
