d020fa3e8ba4db8032d2e803a1f22e02658c0350
Santhosh Cholak <51264445+santhosh2401@users.noreply.github.com>
2019-09-16 22:00:27 +0530

    Add files via upload

---
+++ Home Works/Swing/SwingDemo1.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+public class SwingDemo1  {
+	public static void main(String[] args) {
+		JFrame f = new JFrame("Demo1"); //Creating Objects for JFrame
+		f.setVisible(true); //Methods in JFrame
+		f.setSize(400,400); //Methods in JFrame
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
+
+
+/* 
+import javax.swing.*;
+public class SwingDemo1  {
+	public static void main(String[] args) {
+		 new A();	
+	}
+}
+@SuppressWarnings("serial")
+class A extends JFrame
+{
+	A()
+	{
+		super("Demo1");
+		setVisible(true); //Methods in JFrame
+		setSize(400,400); //Methods in JFrame
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
+*/
+
